8a3e33dd3728cc004d2d31aecfb90ca615b6856d
faiz1844 <ismafaizanidaul.com>
2026-01-21 23:36:54 +0700

    new

--- data_keluaran/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('warga.urls')),  # arahkan ke aplikasi warga
-    path('api/', include('warga.api_urls')),
-]
